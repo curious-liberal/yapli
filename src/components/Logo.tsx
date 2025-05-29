dede01592c8fdfa0530b98344c95fc3a32563495
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-05-29 13:23:33 +0100

    feat: add logo component in bottom right of pages

---
+++ src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <div className="fixed bottom-4 right-4 z-10 opacity-60 hover:opacity-100 transition-opacity">
+      <Image
+        src="/images/zest-logo.png"
+        alt="Zest Logo"
+        width={48}
+        height={48}
+        className="rounded-lg"
+      />
+    </div>
+  );
+}
+
