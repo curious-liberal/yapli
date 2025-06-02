8b71204704e1a9838b73981eaf87f65a796f279c
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-06-02 15:18:53 +0100

    chore: update alt text

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
     <div className="fixed bottom-4 right-4 z-10 opacity-60 hover:opacity-100 transition-opacity">
       <Image
         src="/images/yapli-logo.png"
-        alt="Zest Logo"
+        alt="Yapli Logo"
         width={48}
         height={48}
         className="rounded-lg"
