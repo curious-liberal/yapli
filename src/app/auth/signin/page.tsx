deebd381c75a38006c85759dc191a4fbd1e5f481
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-06-03 08:39:23 +0100

    style: change gradient on sign in / register pages

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -43,7 +43,7 @@ export default function SignIn() {
         <div className="max-w-4xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-0">
-              <h1 className="text-5xl font-bold font-mono bg-gradient-to-r from-[#3EBDC7] to-blue-500 bg-clip-text text-transparent pb-2">
+              <h1 className="text-5xl font-bold font-mono text-yapli-teal ">
                 yapli
               </h1>
               <Image
@@ -117,7 +117,7 @@ export default function SignIn() {
             <button
               type="submit"
               disabled={isLoading}
-              className="relative overflow-hidden w-full px-4 py-2 bg-gradient-to-r from-[#3EBDC7] to-blue-500 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-yapli-teal focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300 cursor-pointer before:absolute before:inset-0 before:bg-gradient-to-r before:from-[#7bcad9] before:to-blue-600 before:opacity-0 before:transition-opacity before:duration-300 hover:before:opacity-100"
+              className="relative overflow-hidden w-full px-4 py-2 bg-gradient-to-r from-yapli-teal to-yapli-dark text-white rounded-md focus:outline-none focus:ring-2 focus:ring-yapli-teal focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
             >
               <span className="relative z-10">
                 {isLoading ? "Signing in..." : "Sign in"}
