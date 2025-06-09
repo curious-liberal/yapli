861cf09c11c2c8108863e1f6421bebdc8b9fc3f4
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-06-09 16:10:49 +0100

    chore: add aria-hidden=true to logo to hide it from screen readers as it's purely decorative

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -39,6 +39,7 @@ export default function Logo({
       height={(size * 595) / 500}
       viewBox="0 0 500 595"
       xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
     >
       <style>
         {`
